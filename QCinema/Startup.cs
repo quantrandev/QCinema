32bbfcc9f6afa3338f975953c07694c6970fd319
Quan Tran <quan081196@gmail.com>
2017-10-14 15:14:19 +0700

    Add project files.

---
+++ QCinema/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QCinema.Startup))]
+namespace QCinema
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
